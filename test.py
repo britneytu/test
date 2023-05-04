e08f5db7db9a8061645aa82d99b49548d239daf0
soar_local_admin <root@localhost>
2023-05-04 17:16:54 +0000

    ss

--- test.py
+++ test.py
@@ -17,6 +17,27 @@ def on_start(container):
     return
 
 @phantom.playbook_block()
+def action_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("action_1() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    parameters = []
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    return
+
+
+@phantom.playbook_block()
 def on_finish(container, summary):
     phantom.debug("on_finish() called")
 
